ebb57f5982f2427c774f7bf8fed8e8beae7da07d
artpumpkin <pumpkin.salah@gmail.com>
2021-02-28 19:24:57 +0100

    feat: update api

--- app/main.py
+++ app/main.py
@@ -1,62 +1,85 @@
 import os
 import requests
 from dotenv import load_dotenv, find_dotenv
-from flask import Flask, jsonify,request
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from .funcs import *
 
 
-
-
 load_dotenv(find_dotenv())
 
-app = Flask(__name__) 
+app = Flask(__name__)
 CORS(app)
 
-@app.route('/info', methods=['GET'])
-def get_info():
-    result={}
 
-    data=request.get_json()
-    key=data['coin']
+@app.route("/info", methods=["GET"])
+def get_info():
+    result = {}
+
+    data = request.get_json()
+    key = data["coin"]
+
     # top tweets [ 10 ] => name,@username,avatar,created_at,idOfTweet
-    topTweets= search(key,"popular",10)
-    result['topTweets']=list(map(lambda x :{key: x[key] for key in x if key not in ["Hashtags","Tweet","Tweet_Location"]},topTweets)) #clean
+    topTweets = search(key, "popular", 10)
+    result["topTweets"] = list(
+        map(
+            lambda x: {
+                key: x[key] for key in x if key not in ["hashtags", "tweet", "location"]
+            },
+            topTweets,
+        )
+    )
+
     # sentiments => compound
-    result_all=search("cardano","recent",10)
-    tweets=[x['Tweet']for x in result_all]
-    
-    sentiments=get_sentiments(tweets) #clean
-    result['compound']=sentiments['com']
-    result['posNeuNeg']=[sentiments['nb_pos'],sentiments['nb_neu'],sentiments['nb_neg']]
-    
-    result['hash_top_freq_data']=hashtag_input(tweets) 
+    result_all = search(key, "recent", 25)
+    tweets = [x["tweet"] for x in result_all]
+
+    sentiments = get_sentiments(tweets)
+    result["score"] = {
+        "positive": sentiments["nb_pos"],
+        "negative": sentiments["nb_neg"],
+        "neutral": sentiments["nb_neu"]
+    }
+
+    result["hashtags"] = hashtag_input(tweets)
+
     # buy hold sell :
-    all_terms=[]
+    all_terms = []
     for i in tweets:
-        all_terms+=get_tokenized_text(i)
- 
-    result['buy_hold_sell_data']=circle_graph_input(get_decision(all_terms))
-    # visualisations => wordcloud + bar_plots
-    result['wordcloud_data']=get_wordCloud(tweets)
-    result['bar_pays_data']=bar_graph_input(result_all)
+        all_terms += get_tokenized_text(i)
+
+    result["buyDecision"] = buy_decision(get_decision(all_terms))
+
+    # visualisations => wordcloud
+    result["wordcloud"] = get_wordCloud(tweets)
     return jsonify(result)
 
 
-@app.route("/coins", methods=['GET']) 
+@app.route("/coins", methods=["GET"])
 def coins():
-    headers = {'X-CMC_PRO_API_KEY': os.environ.get("CMC_API_KEY")}
-    response = requests.get(f'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?aux&sort=cmc_rank', headers=headers)
+    headers = {"X-CMC_PRO_API_KEY": os.environ.get("CMC_API_KEY")}
+    response = requests.get(
+        f"https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?aux&sort=cmc_rank",
+        headers=headers,
+    )
     return jsonify(response.json()["data"])
 
-@app.route("/coins/<string:id>", methods=['GET']) 
-def coin(id: str): 
-    headers = {'X-CMC_PRO_API_KEY': os.environ.get("CMC_API_KEY")}
-    response = requests.get(f'https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?id={id}', headers=headers)
+
+@app.route("/coins/<string:id>", methods=["GET"])
+def coin(id: str):
+    headers = {"X-CMC_PRO_API_KEY": os.environ.get("CMC_API_KEY")}
+    response = requests.get(
+        f"https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?id={id}",
+        headers=headers,
+    )
     return jsonify(response.json()["data"][id])
 
-@app.route("/quotes/<string:id>", methods=['GET']) 
-def quote(id: str): 
-    headers = {'X-CMC_PRO_API_KEY': os.environ.get("CMC_API_KEY")}
-    response = requests.get(f'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?id={id}', headers=headers)
+
+@app.route("/quotes/<string:id>", methods=["GET"])
+def quote(id: str):
+    headers = {"X-CMC_PRO_API_KEY": os.environ.get("CMC_API_KEY")}
+    response = requests.get(
+        f"https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?id={id}",
+        headers=headers,
+    )
     return jsonify(response.json()["data"][id])
